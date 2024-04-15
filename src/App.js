efc654074ee13dcb784ed0f0681b29a240aecde8
vikash <vikashsinghs5530@gmail.comm>
2024-04-15 13:39:31 +0530

    chat-app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Sidebar from './component/Sidebar';
+import Home from './component/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar/>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
